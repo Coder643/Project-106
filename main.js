958ea6a6809327dc1fccae33575411007e794983
Coder643 <79839709+Coder643@users.noreply.github.com>
2022-03-10 19:52:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 //https://teachablemachine.withgoogle.com/models/Fgu7p1nO4/model.json 
+
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/Fgu7p1nO4/model.json ", modelReady());
+}
+
+function modelReady(){
+    classifier.classify(goResults());
+
+}
+
+function goReults(error,results){
+console.log("gotResult");
+}
